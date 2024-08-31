f2f55da77887049533e4e63763a2471f8e6f0452
StephenTan12 <tansteve078@gmail.com>
2024-08-31 09:26:33 -0700

    updated workers

---
+++ app/master.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+type MasterClientData struct {
+	workToWorkerMap   map[int](net.Conn)
+	numWorkers        int
+	maxWorkers        int
+	mutex             sync.Mutex
+	regStageCompleted bool
+}
+
+func createMasterClient() {
+	fmt.Println("created master")
+
+	masterServerIPAddr := LOCAL_HOST_IP_ADDR + ":" + MASTER_SERVER_PORT_STR
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", masterServerIPAddr)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	workerData := MasterClientData{
+		workToWorkerMap:   make(map[int]net.Conn),
+		numWorkers:        0,
+		maxWorkers:        3,
+		mutex:             sync.Mutex{},
+		regStageCompleted: false,
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		handleWorkerRegistration(conn, &workerData)
+
+		if workerData.regStageCompleted {
+			break
+		}
+	}
+
+	sendWordDataToWorkers(&workerData)
+
+	for {
+
+	}
+}
+
+func handleWorkerRegistration(conn net.Conn, workerData *MasterClientData) {
+	data, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	stringData := strings.TrimSpace(data)
+	fmt.Println("received: " + stringData)
+
+	splittedStringData := strings.Split(stringData, " ")
+
+	if workerData.maxWorkers > workerData.numWorkers && splittedStringData[0] == REGISTER_WORKER {
+		ipAddr := IpAddr{
+			ip:   splittedStringData[1],
+			port: splittedStringData[2],
+		}
+
+		workerData.mutex.Lock()
+		workerData.workToWorkerMap[workerData.numWorkers] = conn
+		workerData.numWorkers += 1
+		workerData.mutex.Unlock()
+
+		message := "registered " + strconv.Itoa(workerData.numWorkers) + "\n"
+		conn.Write([]byte(message))
+
+		fmt.Printf("registered worker from %s:%s\n", ipAddr.ip, ipAddr.port)
+
+		if workerData.numWorkers == workerData.maxWorkers {
+			workerData.regStageCompleted = true
+		}
+	}
+}
+
+func sendWordDataToWorkers(workerData *MasterClientData) {
+	f, err := os.Open("../test-files/1000-common-words.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(f)
+	wordPerServer := make([][]string, workerData.numWorkers)
+	currentWordNum := 0
+
+	for scanner.Scan() {
+		word := scanner.Text()
+
+		hashWordKey := hashWord(word)
+		workerNum := getWorkerNum(hashWordKey, workerData.numWorkers)
+
+		wordPerServer[workerNum] = append(wordPerServer[workerNum], word)
+		currentWordNum += 1
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Num of words for server 0: ", len(wordPerServer[0]))
+	fmt.Println("Num of words for server 1: ", len(wordPerServer[1]))
+	fmt.Println("Num of words for server 2: ", len(wordPerServer[2]))
+
+	var wg sync.WaitGroup
+
+	for workerNum, workerWords := range wordPerServer {
+		wg.Add(1)
+		workerConn := workerData.workToWorkerMap[workerNum]
+		go sendDataToWorker(workerConn, workerWords, &wg)
+	}
+
+	wg.Wait()
+}
+
+func hashWord(word string) int {
+	return int(word[0]) - int('a')
+}
+
+func getWorkerNum(hashWordKey int, numWorkers int) int {
+	intervals := 26 / numWorkers
+
+	for numWorker := range numWorkers {
+		if hashWordKey < intervals*(numWorker+1) {
+			return numWorker
+		}
+	}
+	return numWorkers - 1
+}
+
+func sendDataToWorker(workerConn net.Conn, wordsToSend []string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for _, word := range wordsToSend {
+		messageContent := "add " + word + "\n"
+		time.Sleep(3 * time.Millisecond)
+		_, err := workerConn.Write([]byte(messageContent))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	time.Sleep(3 * time.Millisecond)
+	messageContent := "@ \n"
+	_, err := workerConn.Write([]byte(messageContent))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ app/worker.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type WorkerInternalData struct {
+	words              []string
+	mutex              sync.Mutex
+	doneReceivingWords bool
+	masterConn         net.Conn
+	workerNum          int
+}
+
+func createWorkerServer(workerPort string) {
+	workerServerIpAddr := LOCAL_HOST_IP_ADDR + ":" + workerPort
+
+	registrationMessage := "register " + LOCAL_HOST_IP_ADDR + " " + workerPort + "\n"
+	masterIpAddr := LOCAL_HOST_IP_ADDR + ":" + MASTER_SERVER_PORT_STR
+	response, masterConn := writeToServer(masterIpAddr, registrationMessage)
+
+	formattedResponse := strings.Split(strings.TrimSpace(response), " ")
+
+	if formattedResponse[0] != "registered" {
+		fmt.Println("failed to register worker")
+		return
+	}
+
+	workerNum, err := strconv.Atoi(formattedResponse[1])
+	if err != nil {
+		fmt.Println("failed to read worker number")
+		return
+	}
+
+	internalData := WorkerInternalData{
+		words:              make([]string, 0),
+		mutex:              sync.Mutex{},
+		doneReceivingWords: false,
+		masterConn:         masterConn,
+		workerNum:          workerNum,
+	}
+
+	fmt.Printf("worker %s is ready to start accepting\n", workerServerIpAddr)
+
+	for {
+		fmt.Println("trying to read")
+		data, err := bufio.NewReader(internalData.masterConn).ReadString('\n')
+		if err != nil {
+			masterConn.Close()
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("read: " + data)
+		formattedData := strings.Split(strings.TrimSpace(data), " ")
+
+		if formattedData[0] == "add" {
+			internalData.words = append(internalData.words, formattedData[1])
+
+		} else if formattedData[0] == "@" {
+			internalData.doneReceivingWords = true
+			break
+		}
+	}
+
+	fmt.Printf("worker %d has received %d words\n", internalData.workerNum, len(internalData.words))
+
+	fmt.Printf("worker %d is sorting words\n", internalData.workerNum)
+	sort.Strings(internalData.words)
+	fmt.Println(internalData.words)
+}
+
+func writeToServer(ipAddr string, messageContent string) (string, net.Conn) {
+	addr, err := net.ResolveTCPAddr("tcp4", ipAddr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte(messageContent))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	response, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return "", nil
+	}
+
+	return response, conn
+}
